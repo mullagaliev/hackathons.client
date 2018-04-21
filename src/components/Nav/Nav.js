d5451c62311f6275e80a4c3bec6616ee743b4084
Jay Bee <jaybee.codes@gmail.com>
2018-04-21 16:45:59 +0300

    add full screen nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,31 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
 import NavIcon from './NavIcon';
 import Logo from './Logo';
 
-const Nav = (props) => {
+class Nav extends Component {
 
-  return (
-    <nav className="nav">
-      <div className="nav-container">
-        <NavIcon />
-        <Logo />
-      </div>
+  render() {
+    return (
+      <nav className="nav">
+        <div className="nav-nav">
+          <span className="nav-nav__close">&times;</span>
+          <ul className="nav-nav__list">
+            <li className="nav-nav__item"><NavLink className="nav-nav__link" to="/">Home</NavLink></li>
+            <li className="nav-nav__item"><NavLink className="nav-nav__link" to="/leaderboard">Leaderboard</NavLink></li>
+            <li className="nav-nav__item"><NavLink className="nav-nav__link" to="/hackathons">Hackathons</NavLink></li>
+            <li className="nav-nav__item"><NavLink className="nav-nav__link" to="/shop">Shop</NavLink></li>
+          </ul>
+          <Logo />
+        </div>
+        <div className="nav-container">
+            <NavIcon />
+            <Logo />
+        </div>
 
-    </nav>
-  )
+      </nav>
+    )
+  }
 }
 
 export default Nav
