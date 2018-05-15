a8277271dca7fbfdd1fdc0e2e5056ed501fece33
Jay Bee <jaybee.codes@gmail.com>
2018-05-16 01:31:39 +0300

    add login reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,30 +1,31 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import profile from './reducers/profileReducer';
-import sort from './reducers/sortingReducer';
-import { NAV_VISIBLE, NAV_INVISIBLE } from './constants';
+import profile from "./reducers/profileReducer";
+import sort from "./reducers/sortingReducer";
+import login from "./reducers/loginReducer";
 
+import { NAV_VISIBLE, NAV_INVISIBLE } from "./constants";
 
 const initialState = {
   isNavVisible: false
-}
+};
 
-const nav = (state=initialState, action) => {
-
-  switch(action.type) {
+const nav = (state = initialState, action) => {
+  switch (action.type) {
     case NAV_VISIBLE:
-      return {...state, isNavVisible: true};
+      return { ...state, isNavVisible: true };
     case NAV_INVISIBLE:
-      return {...state, isNavVisible: false};
+      return { ...state, isNavVisible: false };
     default:
       return state;
   }
-}
+};
 
 const rootReducer = combineReducers({
   nav,
   profile,
-  sort
+  sort,
+  login
 });
 
 export default rootReducer;
