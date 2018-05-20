27b8a1ae5d9d8f7fb1da0bca23134cd81d6d9dd1
Andrew <andrew@mullagaliev.com>
2018-05-20 04:26:46 +0300

    Добавлено новая вертска меню

--- src/components/LeftMenu/index.js
+++ src/components/LeftMenu/index.js
@@ -2,40 +2,24 @@ import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
-import ProfileIcon from "../../assets/all/profile.svg";
-import LeaderboardIcon from "../../assets/all/forward.svg";
-import AvatarIcon from "../../assets/img/user-avatar.jpg";
-import ExitIcon from "../../assets/all/go back.svg";
 
 const LinkItem = styled(NavLink)`
-  display: flex;
-  flex-flow: row nowrap;
-  align-items: center;
-  height: 60px;
-  line-height: 20px;
-  padding-left: 25px;
-  background-color: transparent;
+  display: block;
+  font-family: "AvenirNextCyr-Regular", sans-serif;
   text-decoration: none;
+  font-size: 18px;
+  color: #ffffff;
+  letter-spacing: 0.06px;
   text-align: center;
+  height: 40px;
+  line-height: 20px;
+  background-color: transparent;
   &:hover {
-    background-color: #ffffff;
-    color: black;
-    img {
-      display: none;
-    }
+    opacity: 0.7;
   }
   &.active {
-    background-color: #ffffff;
-    color: black;
-    img {
-      display: none;
-    }
+    opacity: 0.7;
   }
-  font-family: "Muller", sans-serif;
-  font-weight: 400;
-  font-size: 14px;
-  color: #ffffff;
-  letter-spacing: 0;
 `;
 
 const Icon = styled.img`
@@ -45,58 +29,13 @@ const Icon = styled.img`
   margin-right: 20px;
 `;
 
-const UserProfile = styled.div`
-  position: relative;
-  display: flex;
-  flex-flow: row nowrap;
-  align-items: center;
-  width: 100%;
-  height: 85px;
-  border-bottom: 3px solid #ffffff;
-  padding: 20px;
-  img {
-    height: 40px;
-    width: auto;
-    margin-right: 20px;
-    border-radius: 100%;
-  }
-  span {
-    font-family: "Muller", sans-serif;
-    font-weight: 500;
-    font-size: 14px;
-    color: #ffffff;
-    letter-spacing: 0;
-  }
-  .Profile__exit {
-    position: absolute;
-    right: 20px;
-    height: 20px;
-    width: 20px;
-    background-color: white;
-    cursor: pointer;
-  }
-`;
-
 class LeftMenu extends Component {
   render() {
     return (
       <Fragment>
-        <UserProfile>
-          <img src={AvatarIcon} alt="Avatar" />
-          <span>
-            AI<br />community
-          </span>
-          <span className="Profile__exit" />
-        </UserProfile>
         <nav>
-          <LinkItem to="/myprofile">
-            <Icon src={ProfileIcon} alt="Profile" />
-            My profile
-          </LinkItem>
-          <LinkItem to="/leaderboard">
-            <Icon src={LeaderboardIcon} alt="Leaderboard" />
-            Leaderboard
-          </LinkItem>
+          <LinkItem to="/myprofile">My profile</LinkItem>
+          <LinkItem to="/leaderboard">Leaderboard</LinkItem>
         </nav>
       </Fragment>
     );
